ea1fefcb035570dafc0d0da114e716eac0c2736f
fredrik.zetterlund <fredrik.zetterlund@openpos.tech>
2022-12-01 22:34:19 +0100

    Day one

---
+++ Day01/Program.cs
@@ -0,0 +1,23 @@
+﻿var lines = await File.ReadAllLinesAsync("Input.txt");
+
+int index = 0;
+List<int> values = new() { 0 };
+
+foreach (var line in lines)
+{
+    if (!string.IsNullOrWhiteSpace(line))
+    {
+        values[index] += int.Parse(line);
+    }
+    else
+    {
+        values.Add(0);
+        index++;
+    }
+}
+
+Console.WriteLine($"Part 1: {values.Max()}");
+
+Console.WriteLine($"Part 2:  {values.OrderByDescending(x => x).Take(3).Sum()}");
+
+Console.ReadKey();
