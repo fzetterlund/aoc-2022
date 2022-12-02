f159af6afe1ebe969230e1e8864b219bed161e81
fredrik.zetterlund <fredrik.zetterlund@openpos.tech>
2022-12-02 09:36:15 +0100

    Day 02

---
+++ Day02/Part1.cs
@@ -0,0 +1,54 @@
+﻿namespace Day02;
+
+public static class Part1
+{
+    const int Win = 6;
+    const int Draw = 3;
+    const int Loose = 0;
+
+    const string OpponentRock = "A";
+    const string OpponentPaper = "B";
+    const string OpponentScissors = "C";
+
+    const string Rock = "X";
+    const string Paper = "Y";
+    const string Scissors = "Z";
+
+    private static Dictionary<string, int> selectionScores = new()
+    {
+        { Rock, 1 },
+        { Paper, 2 },
+        { Scissors, 3 },
+    };
+
+    private static Dictionary<string, int> outcomeScoresMap = new()
+    {
+        { $"{OpponentRock} {Scissors}", Loose },
+        { $"{OpponentRock} {Rock}", Draw },
+        { $"{OpponentRock} {Paper}", Win },
+        { $"{OpponentPaper} {Rock}", Loose },
+        { $"{OpponentPaper} {Paper}", Draw },
+        { $"{OpponentPaper} {Scissors}", Win },
+        { $"{OpponentScissors} {Paper}", Loose },
+        { $"{OpponentScissors} {Scissors}", Draw },
+        { $"{OpponentScissors} {Rock}", Win },
+    };
+
+    public static int Calculate(string[] rounds)
+    {
+        var accumulatedScore = 0;
+
+        foreach (var round in rounds)
+        {
+            var choices = round.Split(" ");
+
+            var selectedScore = selectionScores[choices[1]];
+
+            var outcomeScore = outcomeScoresMap[round];
+
+            accumulatedScore += selectedScore + outcomeScore;
+        }
+
+        return accumulatedScore;
+    }
+}
---
+++ Day02/Part2.cs
@@ -0,0 +1,53 @@
+﻿namespace Day02;
+
+public static class Part2
+{
+    const string Win = "Z";
+    const string Draw = "Y";
+    const string Loose = "X";
+
+    const string OpponentRock = "A";
+    const string OpponentPaper = "B";
+    const string OpponentScissors = "C";
+
+    const int Rock = 1;
+    const int Paper = 2;
+    const int Scissors = 3;
+
+    private static Dictionary<string, int> selectedScoresMap = new()
+    {
+        { $"{OpponentRock} {Win}", Paper },
+        { $"{OpponentRock} {Loose}", Scissors },
+        { $"{OpponentRock} {Draw}", Rock },
+        { $"{OpponentPaper} {Win}", Scissors },
+        { $"{OpponentPaper} {Loose}", Rock },
+        { $"{OpponentPaper} {Draw}", Paper },
+        { $"{OpponentScissors} {Win}", Rock },
+        { $"{OpponentScissors} {Loose}", Paper },
+        { $"{OpponentScissors} {Draw}", Scissors },
+    };
+
+    public static int Calculate(string[] rounds)
+    {
+        var accumulatedScore = 0;
+
+        foreach (var round in rounds)
+        {
+            var choices = round.Split(" ");
+
+            var selectedScore = selectedScoresMap[round];
+
+            var outcomeScore = choices[1] switch
+            {
+                "X" => 0,
+                "Y" => 3,
+                "Z" => 6,
+                _ => throw new ArgumentException("Invalid choice")
+            };
+
+            accumulatedScore += selectedScore + outcomeScore;
+        }
+
+        return accumulatedScore;
+    }
+}
