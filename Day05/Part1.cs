73ac56140ff59c93140d02dca2039a58e56bee09
fredrik.zetterlund <fredrik.zetterlund@openpos.tech>
2022-12-05 10:21:01 +0100

    Day 05

---
+++ Day05/Part1.cs
@@ -0,0 +1,55 @@
+﻿namespace Day05;
+
+public static class Part1
+{
+    public static string Calculate(string[] lines)
+    {
+        Dictionary<int, List<string>> items = new();
+        Dictionary<int, Stack<string>> stacks = new();
+
+        var header = lines.TakeWhile(x => !string.IsNullOrWhiteSpace(x)).ToList();
+        var commands = lines.Skip(header.Count + 1);
+
+        foreach (var line in header.SkipLast(1))
+        {
+            int stack = 1;
+            int i = 1;
+            while (i < line.Length)
+            {
+                var stackValue = line.Substring(i, 1);
+                if (!string.IsNullOrWhiteSpace(stackValue))
+                {
+                    if (!items.ContainsKey(stack))
+                        items.Add(stack, new List<string>());
+                    items[stack].Add(stackValue);
+                }
+                i += 4;
+                stack++;
+            }
+        }
+
+        foreach (var item in items)
+        {
+            item.Value.Reverse();
+            stacks.Add(item.Key, new(item.Value));
+        }
+
+        foreach (var command in commands)
+        {
+            var parts = command.Split(" ");
+
+            int count = int.Parse(parts[1]);
+            int from = int.Parse(parts[3]);
+            int to = int.Parse(parts[5]);
+
+            int i = 0;
+            while (i < count)
+            {
+                stacks[to].Push(stacks[from].Pop());
+                i++;
+            }
+        }
+
+        return string.Join(string.Empty, stacks.OrderBy(x => x.Key).Select(x => x.Value.Pop()));
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,6 @@
+﻿var lines = await File.ReadAllLinesAsync("Input.txt");
+
+Console.WriteLine($"Part 1: {Day05.Part1.Calculate(lines)}");
+Console.WriteLine($"Part 2: {Day05.Part2.Calculate(lines)}");
+
+Console.ReadKey();
